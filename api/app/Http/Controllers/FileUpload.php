01dd164671d367b9caa335e00c3089f5eb4ffd8d
Ilya Filisteev <ilya.filisteev@sysqual.net>
2019-05-02 01:35:34 +0300

    change db schema to speed up mass insertion on seeding

--- api/app/Http/Controllers/FileUpload.php
+++ api/app/Http/Controllers/FileUpload.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreFilePost;
 use App\Mail\FileUploaded;
 use App\Models\UserFiles;
-use App\Models\UserHashes;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Mockery\Exception;
 
@@ -27,13 +25,12 @@ class FileUpload extends Controller
             $fileHash = hash_file('md5', $file);
             $userHash = hash('md5', $request->input('fileSenderEmail'));
             $file->storeAs('uploaded'.'/'.$userHash, $fileHash);
-            $userHashes = UserHashes::firstOrCreate(['user_hash' => $userHash]);
             $userFileModel = new UserFiles();
             $userFileModel->description = $request->input('fileDescription');
             $userFileModel->email = $request->input('fileSenderEmail');
-            $userFileModel->user_hash_id = $userHashes->id;
             $userFileModel->file_hash = $fileHash;
-            $userFileModel->filename = $request->fileContent->getClientOriginalName();
+            $userFileModel->user_hash = $userHash;
+            $userFileModel->filename = $file->getClientOriginalName();
             $userFileModel->save();
             Mail::to($userFileModel->email)->send(new FileUploaded($userFileModel));
         } catch (Exception $exception) {
--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+         $this->call(UserHashesTableSeeder::class);
+         $this->call(UserFilesTableSeeder::class);
+         $this->call(UserFilesTestTableSeeder::class);
     }
 }
